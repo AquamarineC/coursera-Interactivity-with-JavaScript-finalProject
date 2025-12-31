badf6aee9d81bd779685654f985139411fa314ac
AquamarineC <224657926+AquamarineC@users.noreply.github.com>
2025-12-30 23:30:48 -0800

    finalprojectinitializing

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+ 
+  document.getElementById("image").innerText = previewPic.alt;
+  document.getElementById("image").style.backgroundImage = `url(${previewPic.src})`;
+
+
+	}
+  
+
+  function unDo(){
+
+    document.getElementById("image").innerText = "Hover over an image below to display here."
+
+    document.getElementById("image").style.backgroundImage = "none"
+  }
