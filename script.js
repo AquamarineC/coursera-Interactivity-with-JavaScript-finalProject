9cb8705551b1c9d098a9724fcf291379659e7906
AquamarineC <224657926+AquamarineC@users.noreply.github.com>
2026-01-14 21:10:03 -0800

    homework update

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 /*Name this external file gallery.js*/
 
+
+
+
 function upDate(previewPic){
  
   document.getElementById("image").innerText = previewPic.alt;
   document.getElementById("image").style.backgroundImage = `url(${previewPic.src})`;
+  
+  document.querySelectorAll('.preview').setAttributes('tabindex', '0');
 
 
 	}
+
+  function accessability(){
+
+  }
   
 
   function unDo(){
@@ -14,4 +23,4 @@ function upDate(previewPic){
     document.getElementById("image").innerText = "Hover over an image below to display here."
 
     document.getElementById("image").style.backgroundImage = "none"
-  }
+  }           
