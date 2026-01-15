e60a04b4b812bb85c709c89eb67af855296dc919
AquamarineC <224657926+AquamarineC@users.noreply.github.com>
2026-01-14 22:35:49 -0800

    console.log scope fix

--- script.js
+++ script.js
@@ -4,9 +4,10 @@
 function accessibility(){
   for (let i = 0; i < document.querySelectorAll('.preview').length; i++) {
     document.querySelectorAll('.preview')[i].setAttribute('tabindex', '0');
+    console.log("loaded image" + i)
+
   }
 
-  console.log("loaded image" + i)
 
 }
    
